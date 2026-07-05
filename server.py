002f2d2e976e90d91b036ae17dc4c05b4eec266c
Marlos Leles <marlosleles@gmail.com>
2026-07-05 10:37:18 -0300

    server.py finished

--- server.py
+++ server.py
@@ -204,3 +204,43 @@ async def documentation_generator(ctx: Context) -> str:
     Raises:
         FileNotFoundError: If the specified file doesn't exist
     """
+    try:
+        result = await ctx.elicit(
+            messge="Please provide the subject file name and the documentation file name",
+            response_type=DocumentGeneratorSchema
+        )
+        file_path = result.data.file_path
+        path = get_path(file_path)
+
+        if not path.exists() or not path.is_file():
+            error_msg = f"Error: {file_path} is not a valid file"
+            await ctx.warning(error_msg)
+            raise FileNotFoundError(error_msg)
+
+        code = path.read_text(encoding='utf-8').strip()
+        language = path.suffix.lower()
+
+        doc_name = result.data.name
+
+        prompt = f"""You are an expert tecnical writer and documentation specialist. Create documentation for the following code file:
+            File: {file_path}
+            Language (file suffix): {language or "Unknown"}
+            Current code:
+                '''
+                {code}
+                '''
+            Use MCP tools available to you to create the separate documentation file
+            - **CRITICAL DETAIL: Name that separate document EXACTLY: {doc_name}**
+            - Add the .md suffix yourself if the name doesn't include it already
+            """.strip()
+
+        await ctx.info("Successfully returned prompt")
+        return prompt
+
+    except Exception as e:
+        await ctx.error(f"Error generating code documentation prompt: {e}")
+        raise
+
+if __name__ == "__main__":
+    print("Starting File Operations Server...")
+    mcp.run()
